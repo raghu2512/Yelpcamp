997fd12ebd3220c1ef28b6682ad08b3753ed226a
raghu2512 <33022727+raghu2512@users.noreply.github.com>
2020-10-07 22:39:46 +0530

    Add existing file

---
+++ app.js
@@ -0,0 +1,59 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var flash = require("connect-flash");
+var passport = require("passport");
+var LocalStrategy = require("passport-local");
+var methodOverride = require("method-override");
+var Campground = require("./models/campground");
+var Comment = require("./models/comment");
+var User = require("./models/user");
+var seedDB = require("./seeds");
+
+//requiring routes
+var commentRoutes = require("./routes/comments");
+var campgroundRoutes = require("./routes/campgrounds");
+var indexRoutes = require("./routes/index");
+
+//seedDB(); //seed the database
+//mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/yelp_camp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+app.use(methodOverride("_method"));
+app.use(flash());
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "I Don't have any secret at all!",
+    resave : false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
+    next();
+});
+
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+
+
+var port  = process.env.port || 3000;
+app.listen(port,  function(){
+    console.log("The YelpCamp Server has Started");
+});
